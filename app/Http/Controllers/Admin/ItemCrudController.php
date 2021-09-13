9c1a602a3b0d0f18dd45b31d4fb173fdb343b5fc
M Wahyu Sanjaya <bukankudabiasa@gmail.com>
2021-09-13 23:02:29 +0700

    fix sorting Item Group & Item Brand

--- app/Http/Controllers/Admin/ItemCrudController.php
+++ app/Http/Controllers/Admin/ItemCrudController.php
@@ -48,6 +48,10 @@ class ItemCrudController extends CrudController
                 'name' => 'brand_id',
                 'type' => 'select_from_array',
                 'options' => ItemBrand::pluck('name', 'id'),
+                'orderLogic' => function ($query, $column, $columnDirection) {
+                    return $query->leftJoin('item_brands', 'items.brand_id', '=', 'item_brands.id')
+                        ->orderBy('item_brands.name', $columnDirection)->select('items.*');
+                }
             ],
             [
                 'label' => 'Code',
